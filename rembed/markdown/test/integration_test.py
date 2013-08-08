214755e91729d2cd162a78b22f432e6e9b437096
Matt Thomson <matt.thomson@cantab.net>
2013-08-08 22:17:51 +0100

    Undo refactoring of import

--- rembed/markdown/test/integration_test.py
+++ rembed/markdown/test/integration_test.py
@@ -1,11 +1,11 @@
-from rembed.markdown import extension
+from rembed.markdown.extension import REmbedExtension
 import markdown
 
 from hamcrest import assert_that, contains_string
 
 
 def test_should_get_correct_embedding():
-    md = markdown.Markdown(extensions=[extension.REmbedExtension()])
+    md = markdown.Markdown(extensions=[REmbedExtension()])
     embedding = md.convert(
         '[!rembed](https://twitter.com/BarackObama/status/266031293945503744)')
     assert_that(embedding, contains_string('Four more years.'))
