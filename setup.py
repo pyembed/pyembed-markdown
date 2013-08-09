37f92e14e3463696d910261c0a7923d347e18737
Matt Thomson <matt.thomson@cantab.net>
2013-08-09 20:25:47 +0100

    Fix PyPI packaging problem

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
 setup(
     name='rembed-markdown',
-    version='0.1.0',
+    version='0.1.1',
     author='Matt Thomson',
     author_email='matt.thomson@cantab.net',
     url='https://github.com/matt-thomson/rembed-markdown',
