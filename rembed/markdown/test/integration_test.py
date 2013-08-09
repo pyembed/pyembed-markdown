582a27661d6852e0dc08f2ec1a165910d63d8c88
Matt Thomson <matt.thomson@cantab.net>
2013-08-09 19:58:01 +0100

    Add attempt at loading extension by name
    Includes failing integration test - issue #2

--- rembed/markdown/test/integration_test.py
+++ rembed/markdown/test/integration_test.py
@@ -2,10 +2,18 @@ from rembed.markdown.extension import REmbedExtension
 import markdown
 
 from hamcrest import assert_that, contains_string
+import pytest
 
 
 def test_should_get_correct_embedding():
     md = markdown.Markdown(extensions=[REmbedExtension()])
+    embedding = md.convert(
+        '[!rembed](https://twitter.com/BarackObama/status/266031293945503744)')
+    assert_that(embedding, contains_string('Four more years.'))
+
+@pytest.mark.xfail
+def test_should_get_correct_embedding_when_initializing_by_name():
+    md = markdown.Markdown(extensions=['rembed.markdown'])
     embedding = md.convert(
         '[!rembed](https://twitter.com/BarackObama/status/266031293945503744)')
     assert_that(embedding, contains_string('Four more years.'))
