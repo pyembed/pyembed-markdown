aa844a0d342a43ff71c9bd7d05a99f813e83e294
Matt Thomson <matt.thomson@cantab.net>
2013-10-26 21:53:00 +0100

    Release v0.1.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
 setup(
     name='rembed-markdown',
-    version='0.1.2',
+    version='0.1.3',
     author='Matt Thomson',
     author_email='matt.thomson@cantab.net',
     url='https://github.com/matt-thomson/rembed-markdown',
