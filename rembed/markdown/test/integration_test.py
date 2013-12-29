49c256847fd003acce90c40fc1468543d43754d8
Matt Thomson <matt.thomson@cantab.net>
2013-12-29 15:25:44 +0000

    Allow specifying maximum height / width
    Closes #5

--- rembed/markdown/test/integration_test.py
+++ rembed/markdown/test/integration_test.py
@@ -15,6 +15,16 @@ def test_should_get_correct_embedding():
     assert_that(embedding, is_not(contains_string('&gt;')))
 
 
+def test_should_embed_with_max_height():
+    md = markdown.Markdown(extensions=[REmbedExtension()])
+
+    embedding = md.convert(
+        '[!embed?max_height=200](http://www.youtube.com/watch?v=9bZkp7q19f0)')
+
+    assert_that(embedding, contains_string('height="200"'))
+    assert_that(embedding, is_not(contains_string('&gt;')))
+
+
 @pytest.mark.xfail
 def test_should_get_correct_embedding_when_initializing_by_name():
     md = markdown.Markdown(extensions=['rembed.markdown'])
